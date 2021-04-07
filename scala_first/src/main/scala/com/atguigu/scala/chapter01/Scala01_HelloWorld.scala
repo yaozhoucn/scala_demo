a5db759cda662684f4c51b5660d603ec7609ece5
yaozhoucn <wangxiao_h@126.com>
2021-04-07 10:27:32 +0800

    Initial commit

---
+++ scala_first/src/main/scala/com/atguigu/scala/chapter01/Scala01_HelloWorld.scala
@@ -0,0 +1,19 @@
+package com.atguigu.scala.chapter01
+
+/**
+  * Created by WXHang on HANG at 2021/4/7 9:44
+  */
+object Scala01_HelloWorld {
+  def main(args: Array[String]): Unit = {
+    /**
+      * main：主方法
+      * args:参数名称
+      * Array：参数类型
+      * String：string类型数据--指定泛型
+      * Unit：返回值类型（返回值为空）
+      */
+    println("Hello Scala")
+    System.out.println("hello_world")
+  }
+
+}
