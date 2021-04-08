ddaff22e116ee5698e6a56b53c6d9d94ea1e2792
yaozhoucn <wangxiao_h@126.com>
2021-04-08 16:43:09 +0800

    dataType

---
+++ scala_first/src/main/scala/com/atguigu/scala/chapter04/Scala01_If.scala
@@ -0,0 +1,20 @@
+package com.atguigu.scala.chapter04
+
+import scala.io.StdIn
+
+/**
+  * Created by WXHang on HANG at 2021/4/8 16:26
+  */
+object Scala01_If {
+  def main(args: Array[String]): Unit = {
+    println("请输入您的年龄：")
+    val age = StdIn.readInt()
+    if (age < 18){
+      println("童年")
+    }else if(age >= 18 && age < 30){
+      println("成年")
+    }else{
+      println("老年")
+    }
+  }
+}
