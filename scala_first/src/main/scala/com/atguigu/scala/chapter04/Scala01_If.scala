a97f3ccd9163552b7a18ac2fc53cd0d06e461167
yaozhoucn <wangxiao_h@126.com>
2021-04-08 17:53:37 +0800

    dataType

--- scala_first/src/main/scala/com/atguigu/scala/chapter04/Scala01_If.scala
+++ scala_first/src/main/scala/com/atguigu/scala/chapter04/Scala01_If.scala
@@ -8,13 +8,16 @@ import scala.io.StdIn
 object Scala01_If {
   def main(args: Array[String]): Unit = {
     println("请输入您的年龄：")
-    val age = StdIn.readInt()
-    if (age < 18){
-      println("童年")
-    }else if(age >= 18 && age < 30){
-      println("成年")
-    }else{
-      println("老年")
-    }
+   val age = StdIn.readInt()
+//    if (age < 18){
+//      println("童年")
+//    }else if(age >= 18 && age < 30){
+//      println("成年")
+//    }else{
+//      println("老年")
+//    }
+    //案例：通过if else模拟三元运算符实现，res = 条件表达式？ 值1：值2
+    val res:Any = if(age < 18) "童年" else "成年"
+    println(res)
   }
 }
