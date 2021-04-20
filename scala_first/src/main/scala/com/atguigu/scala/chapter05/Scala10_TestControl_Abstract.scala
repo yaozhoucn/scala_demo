a7dd60c12e2b15376e8efa445bbe3bdfbe8f34dc
HANG <wangxiao_h@126.com>
2021-04-20 23:16:08 +0800

    PC

--- scala_first/src/main/scala/com/atguigu/scala/chapter05/Scala10_TestControl_Abstract.scala
+++ scala_first/src/main/scala/com/atguigu/scala/chapter05/Scala10_TestControl_Abstract.scala
@@ -4,5 +4,13 @@ package com.atguigu.scala.chapter05
   * Created by WXHang on HANG at 2021/4/20 17:59
   */
 object Scala10_TestControl_Abstract {
+  /**
+   * desc：控制抽象
+   *  - 值调用
+   *  将函数计算后的值进行传递
+   *
+   *  -名调用
+   *   传递是代码块
+   */
 
 }
