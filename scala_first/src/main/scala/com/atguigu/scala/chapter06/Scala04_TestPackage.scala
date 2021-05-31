e33c002c38acb9661264b4f2b78ec34a10f22843
HANG <wangxiao_h@126.com>
2021-05-31 22:48:55 +0800

    PC

---
+++ scala_first/src/main/scala/com/atguigu/scala/chapter06/Scala04_TestPackage.scala
@@ -0,0 +1,18 @@
+package com.atguigu.scala.chapter06
+
+/**
+ * Created by WXHang on HANG at 2021/5/31 22:40
+ * desc:import 导包
+ */
+object Scala04_TestPackage {
+  /**
+   * 1.和java一样，可以在顶部导入，在这个文件夹中的所有类都可以使用
+   * 2.局部导入：什么时候使用，什么时候导入。在其作用范围内都可以使用
+   * 3.通配符导入：import java.util._
+   * 4.给类起名：import java.util.{ArrayList=>JL}，jl为新起的别名
+   * 5.屏蔽类：import java.util.{ArrayList =>_,_}
+   * 6.导入相同包的多个类：import java.util.{HashSet, ArrayList}
+   * 7.导入包的绝对路径：new _root_.java.util.HashMap
+   */
+
+}
