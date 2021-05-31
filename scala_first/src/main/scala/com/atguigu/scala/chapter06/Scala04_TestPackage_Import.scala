c60a5d20b2dfae00683ce5d884a27beff73019a0
HANG <wangxiao_h@126.com>
2021-05-31 22:57:51 +0800

    PC

--- scala_first/src/main/scala/com/atguigu/scala/chapter06/Scala04_TestPackage_Import.scala
+++ scala_first/src/main/scala/com/atguigu/scala/chapter06/Scala04_TestPackage_Import.scala
@@ -4,7 +4,7 @@ package com.atguigu.scala.chapter06
  * Created by WXHang on HANG at 2021/5/31 22:40
  * desc:import 导包
  */
-object Scala04_TestPackage {
+object Scala04_TestPackage_Import {
   /**
    * 1.和java一样，可以在顶部导入，在这个文件夹中的所有类都可以使用
    * 2.局部导入：什么时候使用，什么时候导入。在其作用范围内都可以使用
