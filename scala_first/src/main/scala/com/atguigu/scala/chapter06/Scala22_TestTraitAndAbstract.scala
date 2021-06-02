d8c56dc2816cd61efd3e7c3e4379d41c346f6019
yaozhoucn <wangxiao_h@126.com>
2021-06-02 16:53:39 +0800

    特质(接口)

---
+++ scala_first/src/main/scala/com/atguigu/scala/chapter06/Scala22_TestTraitAndAbstract.scala
@@ -0,0 +1,16 @@
+package com.atguigu.scala.chapter06
+
+/**
+  * Created by WXHang on HANG at 2021/6/2 16:48
+  * Desc：抽象类中可以定义抽象属性，抽象方法，非抽象属性，非抽象方法
+  * 特质中可以定义抽象属性，抽象方法，非抽象属性，非抽象方法
+  * 抽象类和特质都不能被实例化
+  *   >抽象类
+  */
+object Scala22_TestTraitAndAbstract {
+
+}
+
+trait Trait22{
+  println("特质的构造方法")
+}
