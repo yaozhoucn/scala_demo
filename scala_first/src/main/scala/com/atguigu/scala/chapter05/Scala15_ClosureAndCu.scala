e1614a6d75246a8819dbdc717cad45161573aaad
HANG <wangxiao_h@126.com>
2021-11-01 14:27:35 +0800

    特质(接口)

--- scala_first/src/main/scala/com/atguigu/scala/chapter05/Scala15_ClosureAndCu.scala
+++ scala_first/src/main/scala/com/atguigu/scala/chapter05/Scala15_ClosureAndCu.scala
@@ -12,7 +12,7 @@ object Scala15_ClosureAndCu {
       def f2(b:Int):Int = {
           a + b
       }
-      //f2()作为f1（）的返回值进行传递
+      //f2()作为f1()的返回值进行传递
       f2 _
     }
     //调用f1，返回的是f2,此处 2 为f2（2）传值参数
