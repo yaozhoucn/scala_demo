471bb6521e0b45074dbef686a9282026c269c5da
HANG <wangxiao_h@126.com>
2022-01-06 18:42:52 +0800

    PC

--- scala_first/src/main/scala/com/atguigu/scala/chapter07/Scala07_TestMap.scala
+++ scala_first/src/main/scala/com/atguigu/scala/chapter07/Scala07_TestMap.scala
@@ -1,5 +1,7 @@
 package com.atguigu.scala.chapter07
 
+import scala.collection.mutable
+
 /**
  * Created by WXHang on HANG at 2022/1/6 17:56
  * desc:Map集合
@@ -26,10 +28,22 @@ object Scala07_TestMap {
     println(map.get("d"))
 
     //如果这个值为空则赋一个默认值
-    println("======"+map.get("f").getOrElse(0))
+    //println("======"+map.get("f").getOrElse(0))
+    println(map.getOrElse("f", 66666))
     //获取map中的value
      for (value <- map.values){
        println(value)
      }
+
+    //创建可变的Map集合
+    val map1: mutable.Map[String, Int] = mutable.Map("a" -> 1, "b" -> 2, "c" -> 3)
+    //添加元素
+    map1.put("d",4)
+    //修改元素
+    map1.update("c",5)
+    map1("a")=4
+    //删除元素
+    map1.remove("a")
+    map1.foreach(println)
   }
 }
