62abcb4ed4264059aa2a43673437256c8574e722
HANG <wangxiao_h@126.com>
2022-01-12 15:49:54 +0800

    PC

--- scala_first/src/main/scala/com/atguigu/scala/chapter07/Scala12_TestFun.scala
+++ scala_first/src/main/scala/com/atguigu/scala/chapter07/Scala12_TestFun.scala
@@ -15,17 +15,26 @@ object Scala12_TestFun {
 //    将集合中的每一个元素映射到某一个函数
     println(list.map(elem => elem * 2))
     println(list.map(_ * 2))
-//    （3）扁平化 :::
+//    （3）扁平化 flatten :::
     val nestList = List(List(1, 2, 4), List(3, 4, 5), List(8, 9, 10))
     nestList.flatten.foreach(print)
     //    （4）扁平化+映射 注：flatMap相当于先进行map操作，在进行flatten操作
     //    集合中的每个元素的子元素映射到某个函数并返回新集合
     val stringList: List[String] = List("hello atguigu", "hello bobo", "hello xian", "hello yaozhou")
-    println(stringList.flatMap(s => s.split(" ")))
+    //stringList.flatMap(s => s.split(" ")).foreach(println)
+    //val flatten: List[String] = stringList.map(elem => elem.split(" ")).flatten
+    //flatten.foreach(println)
+    val stringList2: List[Array[String]] = stringList.map(elem => elem.split(" "))
+    println(stringList2.flatten)
 //    （5）分组(group)
 //    按照指定的规则对集合的元素进行分组
+    println(list.groupBy(elem => elem % 2==0))
+
+    val stringLists: List[String] = List("test", "type", "xian", "xiye", "yaozhou", "bobo", "but")
+    val groupByMap: Map[Char, List[String]] = stringLists.groupBy(elem => elem.charAt(0))
+    println(groupByMap)
 //    （6）简化（归约）
-//
+
 //    （7）折叠
   }
 
---
+++ scala_first/src/main/scala/com/atguigu/scala/chapter07/Scala13_TestFun.scala
@@ -0,0 +1,25 @@
+package com.atguigu.scala.chapter07
+
+/**
+ * Created by WXHang on HANG at 2022/1/12 12:52
+ * desc: 简化（归约）-通过指定的逻辑将集合中的数据进行聚合，从而减少数据，最终获取结果。
+ *   > reduce底层调用的的是reduceLeft
+ *   > 函数参数类型以及返回值类型必须一致
+ *   > reduceLeft 函数参数类型可以不一致
+ *   > reduceRight 函数参数类型可以不一致
+ *
+ * -折叠
+ */
+object Scala13_TestFun {
+  def main(args: Array[String]): Unit = {
+    val list = List(1, 2, 3, 4)
+    //println(list.reduce((x, y) => x - y))
+    val res: Int = list.reduceLeft((a: Int, b: Int) => {
+      a + b
+    })
+    println(res)
+    println(list.reduceLeft(_ + _))
+    println(list.reduceRight(_ + _))
+  }
+
+}
